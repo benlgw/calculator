ebed9064029bb660457170db431afb040de780d0
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-02 10:16:19 +0100

    added operate function which calls correct math function based on operator

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
 	return num1 / num2;
 }
+
+function operate(num1, operator, num2) {
+	switch (operator) {
+		case "+":
+			const addition = add(num1, num2);
+			break;
+		case "-":
+			const subtraction = subtract(num1, num2);
+			break;
+		case "*":
+			const multiplication = multiply(num1, num2);
+			break;
+		case "/":
+			const division = divide(num1, num2);
+			break;
+	}
+}
