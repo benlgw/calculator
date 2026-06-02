13301aabfdd8dec2c805db9e114dbe33308bdc0e
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-02 11:03:05 +0100

    added event listener for all the buttons

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const maths = document.getElementById("maths");
+const result = document.getElementById("result");
+
+let num1;
+let operator;
+let num2;
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((element) => {
+	element.addEventListener("click", () => {
+		alert(`Clicked ${element.textContent}`);
+	});
+});
+
 function add(num1, num2) {
 	return num1 + num2;
 }
